c55cb7f67596faecbdef5fbba285880163457eae
Mo'men Sherif <momensherif.2019@gmail.com>
2022-11-26 10:36:27 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex flex-col justify-center items-center bg-gray-900 text-white min-h-screen">
+      <h1 className="text-9xl font-bold bg-gradient-to-r from-blue-500 to-green-500 bg-clip-text text-transparent drop-shadow-xl">
+        Hello, DND Kit
+      </h1>
+    </div>
+  );
+}
