f579cf4bf16a8fdb9ce8d1e820733df52cff6301
Mo'men Sherif <momensherif.2019@gmail.com>
2022-11-28 21:22:56 +0200

    Sortable with dragoverlay

--- src/Examples/Sortable/1.Sortable.tsx
+++ src/Examples/Sortable/1.Sortable.tsx
@@ -1,8 +1,10 @@
 import {
   closestCorners,
   DndContext,
+  DragOverlay,
   KeyboardSensor,
   PointerSensor,
+  useDndContext,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
@@ -16,6 +18,7 @@ import {
 } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import TodoItem, { TodoItemProps } from './TodoItem';
 
 const todos = Array.from({ length: 10 }, (_, idx) => ({
@@ -25,6 +28,8 @@ const todos = Array.from({ length: 10 }, (_, idx) => ({
 
 export default function SortableExample() {
   const [items, setItems] = useState(todos);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -36,7 +41,12 @@ export default function SortableExample() {
     <DndContext
       sensors={sensors}
       collisionDetection={closestCorners}
-      modifiers={[restrictToVerticalAxis]}
+      // modifiers={[restrictToVerticalAxis]}
+      onDragStart={event => {
+        const { active } = event;
+        setActiveIndex(active ? active.data.current?.sortable.index : null);
+      }}
+      onDragCancel={() => setActiveIndex(null)}
       onDragEnd={event => {
         const { active, over } = event;
         if (active.id === over?.id) return;
@@ -45,6 +55,7 @@ export default function SortableExample() {
         const overIndex = over?.data.current?.sortable.index;
 
         setItems(prevItems => arrayMove(prevItems, activeIndex, overIndex));
+        setActiveIndex(null);
       }}
     >
       <SortableContext items={items} strategy={verticalListSortingStrategy}>
@@ -54,24 +65,44 @@ export default function SortableExample() {
           ))}
         </div>
       </SortableContext>
+      <DraggableOverlay {...(activeIndex !== null ? items[activeIndex] : {})} />
     </DndContext>
   );
 }
 
 function SortableTodoItem(props: TodoItemProps) {
-  const { setNodeRef, attributes, listeners, transform, transition } =
-    useSortable({
-      id: props.id,
-    });
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: props.id,
+  });
 
   return (
     <TodoItem
       ref={setNodeRef}
       style={{ transition, transform: CSS.Transform.toString(transform) }}
-      className="focus:outline-none focus:ring"
+      className={`focus:outline-none focus:ring ${
+        isDragging ? 'opacity-50' : ''
+      }`}
       {...props}
       {...attributes}
       {...listeners}
     />
+  );
+}
+
+function DraggableOverlay(props: any) {
+  const { active } = useDndContext();
+
+  return createPortal(
+    <DragOverlay>
+      {active && <TodoItem {...props} className="scale-110 shadow-2xl" />}
+    </DragOverlay>,
+    document.body,
   );
 }
